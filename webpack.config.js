84cb6245e9f22868aafcc52a5e1f641da9522c6c
RY lin <1019022357@qq.com>
2019-05-29 22:34:09 +0800

    Webpack 4 打包配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,64 @@
-const webpack = require("webpack");
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    devtool: "eval-source-map", //调试工具类型
+  devtool: 'eval-source-map', //调试工具类型
 
-    entry: __dirname + "/app/main.js", //入口文件
+  entry: __dirname + '/src/index.js', //入口文件
 
-    output: {
-        path: __dirname + "/build", //文件打包之后的地址
-        filename: "bundle.js" //打包的文件名
-    },
+  output: {
+    path: __dirname + '/build', //文件打包之后的地址
+    filename: '[name].js' //打包的文件名
+  },
 
-    //在本地启动服务器
-    devServer: {
-        contentBase: path.join(__dirname, "build"), //本地服务器加载的页面所在目录
-        historyApiFallback: true, //所有页面跳转都回到index.html
-        inline: true, //热加载
-        port: 8080 //默认端口
-    },
+  //在本地启动服务器, 打包文件的相关内容是存储在内存之中的，所以文件夹中没有 build 文件
+  devServer: {
+    contentBase: path.join(__dirname, 'build'), //本地服务器加载的页面所在目录
+    historyApiFallback: true, //所有页面跳转都回到index.html
+    inline: true, //热加载
+    port: 8080, //默认端口
+    proxy: {
+      '/api': {
+        target: 'http:www.xxx.com'
+      }
+    }
+  },
 
-    //loaders
-    module: {
-        rules: [
-            {
-                test: /(\.jsx|\.js)$/,
-                use: {
-                    loader: "babel-loader"
-                },
-                exclude: /node_modules/ //不去匹配 node_modules 下的文件
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: true, // 指定启用css modules
-                            localIdentName: "[name]__[local]--[hash:base64:5]" // 指定css的类名格式
-                        }
-                    },
-                    {
-                        loader: "postcss-loader"
-                    }
-                ]
-            }
-        ]
-    },
-
-    //插件
-    plugins: [
-        new webpack.BannerPlugin("I am Glory"),
-        new HtmlWebpackPlugin({
-            template: __dirname + "/index.html" //new 一个这个插件的实例，并传入相关参数
-        })
+  //loaders
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        use: {
+          loader: 'babel-loader'
+        },
+        exclude: /node_modules/ //不去匹配 node_modules 下的文件
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            outputPath: 'images/',
+            limit: 5 * 1024
+          }
+        }
+      }
     ]
+  },
+
+  //插件
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/index.html',
+      hash: true,
+      minify: {
+        removeAttributeQuotes: true
+      }
+    }) //以当前目录的 index.html 为模板生成打包的 index.html
+  ]
 };
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,72 +1,52 @@
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    devtool: "eval-source-map", //调试工具类型
+  entry: __dirname + '/src/index.js', //入口文件
 
-    entry: __dirname + "/app/main.js", //入口文件
+  output: {
+    path: __dirname + '/build', //文件打包之后的地址
+    filename: '[name].[hash: 7].js' //打包的文件名，加上哈希值，去掉缓存
+  },
 
-    output: {
-        path: __dirname + "/build", //文件打包之后的地址
-        filename: "bundle.js-[hash].js" //打包的文件名，加上哈希值，去掉缓存
-    },
+  //loaders
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        use: {
+          loader: 'babel-loader'
+        },
+        exclude: /node_modules/ //不去匹配 node_modules 下的文件
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.(png|jpg|jpge|gif|svg)/,
+        use: {
+          loader: 'url-loader',
+          limit: 5 * 1024
+        }
+      }
+    ]
+  },
 
-    //loaders
-    module: {
-        rules: [
-            {
-                test: /(\.jsx|\.js)$/,
-                use: {
-                    loader: "babel-loader"
-                },
-                exclude: /node_modules/ //不去匹配 node_modules 下的文件
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: true, // 指定启用css modules
-                            localIdentName: "[name]__[local]--[hash:base64:5]" // 指定css的类名格式
-                        }
-                    },
-                    {
-                        loader: "postcss-loader"
-                    }
-                ]
-            }
-        ]
-    },
-
-    //插件
-    plugins: [
-        new webpack.BannerPlugin("I am Glory"), //打包时候加进入的文字
-        new HtmlWebpackPlugin({
-            template: __dirname + "/index.html" //new 一个这个插件的实例，并传入相关参数
-        }),
-        new webpack.optimize.OccurrenceOrderPlugin(), //为组件分配ID，通过这个插件webpack可以分析和优先考虑使用最多的模块，并为它们分配最小的ID
-        new ExtractTextPlugin("style.css"), //分离CSS和JS文件
-        new CleanWebpackPlugin("build/*.*", {
-            root: __dirname,
-            verbose: true,
-            dry: false
-        }) //清除上一个 bundle 文件
-    ],
-
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                uglifyOptions: {
-                    compress: false
-                }
-            })
-        ]
-    } //压缩JS代码
+  //插件
+  plugins: [
+    new webpack.BannerPlugin('I am Glory'), //打包时候加进入的文字
+    new HtmlWebpackPlugin({
+      template: __dirname + '/index.html',
+      hash: true,
+      minify: {
+        removeAttributeQuotes: true
+      }
+    }), //以当前目录的 index.html 为模板生成打包的 index.html
+    new webpack.optimize.OccurrenceOrderPlugin(), //为组件分配ID，通过这个插件webpack可以分析和优先考虑使用最多的模块，并为它们分配最小的ID
+    new ExtractTextPlugin('style.css'), //分离CSS和JS文件
+    new CleanWebpackPlugin() //清除上一个 build 文件
+  ]
 };
